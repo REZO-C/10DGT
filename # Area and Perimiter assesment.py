c14cd07d91654fc9c6e9d522fa30ce7a7b535828
Caleb <caleb.prestidge@kingsway.school.nz>
2025-05-26 09:05:50 +1200

    Update # Area and Perimeter assesment.py

--- # Area and Perimiter assesment.py
+++ # Area and Perimiter assesment.py
@@ -3,36 +3,37 @@
 # 16/5/2025
 # Version 1
 
-Height = 0
-Width = 0
+height = 0
+width = 0
      
-    # This asks for the Height and Width
-Height = int(input("Please enter the Height: "))
+    # This asks for the Height
+height = float(input("Please enter the Height: "))
+    # This asks for the Width
 
      # tests the height if it is above 0
-while Height <=0:
-    Height = int(input("Error: Please enter the Height (the number must be above 0): "))
+while height <=0:
+    height = float(input("Error: Please enter the Height (the number must be above 0): "))
 
-Width = int(input("Please enter the Width: "))   
-
+width = float(input("Please enter the Width: "))     
      # tests the Width if it is above 0
-while Width <= 0:
-    Width = int(input("Error: Please enter the Width (the number must be above 0): "))
+while width <= 0:
+    width = float(input("Error: Please enter the Width (the number must be above 0): "))
 
 
     # Printing the chosen Height and Width
-print(f"The height you chose was: {Height}")
-print(f"The width you  chose was: {Width}")
+print(f"The height you chose was: {height}")
+print(f"The width you  chose was: {width}")
 
     # Calculates and displays the Area
-Area = Height * Width
-print(f"This is the area {Area}")
+area = round ((height * width))
+print(f"This is the area {area}")
 
     # Calculates and displays the Perimeter
-Perimeter = Height + Width + Height + Width
-print(f"This is the Perimeter {Perimeter}")
+perimeter = round((height + width + height + width),2)
+print(f"This is the perimeter {perimeter}")
 
-     # When you press enter the code quits
+
+ # When you press enter the code quits
 user_input = input(f"Press 'enter' to quit")
 if user_input == 'enter':
     exit()
